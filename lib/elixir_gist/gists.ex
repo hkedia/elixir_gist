5fb6f35cb0bd2653ca8431f1d85b36172283e8a0
Harsh Kedia <hkedia@hey.com>
2024-05-31 11:58:51 +0530

    modify schema and contexts

--- lib/elixir_gist/gists.ex
+++ lib/elixir_gist/gists.ex
@@ -49,8 +49,9 @@ defmodule ElixirGist.Gists do
       {:error, %Ecto.Changeset{}}
 
   """
-  def create_gist(attrs \\ %{}) do
-    %Gist{}
+  def create_gist(user, attrs \\ %{}) do
+    user
+    |> Ecto.build_assoc(:gists)
     |> Gist.changeset(attrs)
     |> Repo.insert()
   end
@@ -145,8 +146,9 @@ defmodule ElixirGist.Gists do
       {:error, %Ecto.Changeset{}}
 
   """
-  def create_saved_gist(attrs \\ %{}) do
-    %SavedGist{}
+  def create_saved_gist(user, attrs \\ %{}) do
+    user
+    |> Ecto.build_assoc(:saved_gists)
     |> SavedGist.changeset(attrs)
     |> Repo.insert()
   end
