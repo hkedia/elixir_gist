2e92a55d62144e838a82c5834a9885e1f2248c48
Harsh Kedia <hkedia@hey.com>
2024-06-03 02:12:55 +0530

    remove the heex template file and use the render function instead

--- lib/elixir_gist_web/live/create_gist_live.ex
+++ lib/elixir_gist_web/live/create_gist_live.ex
@@ -6,4 +6,15 @@ defmodule ElixirGistWeb.CreateGistLive do
   def mount(_params, _session, socket) do
     {:ok, socket}
   end
+
+  def render(assigns) do
+    ~H"""
+    <div class="em-gradient flex items-center justify-center">
+      <h1 class="font-brand font-bold text-3xl text-white">
+        Instantly share Elixir code, notes, and snippets.
+      </h1>
+    </div>
+    <.live_component module={GistFormComponent} id="new" current_user={@current_user} />
+    """
+  end
 end
