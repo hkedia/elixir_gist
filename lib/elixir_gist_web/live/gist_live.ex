64071909d50f01fd89fbee3eaf06936b2e8b7236
Harsh Kedia <hkedia@hey.com>
2024-06-02 18:51:10 +0530

    use timex to show relative timestamp

--- lib/elixir_gist_web/live/gist_live.ex
+++ lib/elixir_gist_web/live/gist_live.ex
@@ -4,6 +4,10 @@ defmodule ElixirGistWeb.GistLive do
 
   def mount(%{"id" => id}, _session, socket) do
     gist = Gists.get_gist!(id)
+
+    {:ok, relative_time} = Timex.format(gist.updated_at, "{relative}", :relative)
+    gist = Map.put(gist, :relative, relative_time)
+
     {:ok, assign(socket, gist: gist)}
   end
 
